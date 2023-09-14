911038005a490ce5eda1233f898e82b3db627a15
enmarcm <theenmanuel123@gmail.com>
2023-09-13 22:40:56 -0400

    Estructura

--- models/local/movieModel.js
+++ models/local/movieModel.js
@@ -3,7 +3,7 @@ const movies = importJson({ pathJson: "../json/movies.json" });
 const genreId = importJson({ pathJson: "../json/genre-id.json" });
 
 class MovieModel {
-  static getAll = async () => {
+  static getAll = async ({ genre, page, limit }) => {
     const generosMap = genreId.reduce((obj, elemento) => {
       if (elemento.id === undefined) return;
       obj[elemento.id] = elemento.name;
@@ -18,7 +18,10 @@ class MovieModel {
     return peliculasConGeneros;
   };
 
-  
+  static #getGenre = ({ genre }) => {};
+
+  static #getLimit = ({ page, limit }) => {};
+
   static getByID = async () => {};
   static delete = async () => {};
   static update = async () => {};
