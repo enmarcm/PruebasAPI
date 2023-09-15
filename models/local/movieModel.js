75e8073d66855f482c5236d16fcba99c36cb8374
enmarcm <theenmanuel123@gmail.com>
2023-09-14 20:18:24 -0400

    Documentacion de MovieModel JSON

--- models/local/movieModel.js
+++ models/local/movieModel.js
@@ -1,8 +1,16 @@
+//@ts-check
 import importJson from "../../utils/importJson.js";
 const movies = importJson({ pathJson: "../json/movies.json" });
 const genreId = importJson({ pathJson: "../json/genre-id.json" });
 
+/**
+ * Es un modelo que maneja los datos de las peliculas en formato JSON, archivo movies.json
+ */
 class MovieModel {
+  /**
+   * Permite obtener todas las peliculas con sus generos especificos y no solo con ID - PRIVADA
+   * @returns {Promise<Array<Object>>}
+   */
   static #getMoviGen = async () => {
     const generosMap = genreId.reduce((obj, elemento) => {
       if (elemento.id === undefined) return;
@@ -18,7 +26,16 @@ class MovieModel {
     return peliculasConGeneros;
   };
 
+  /**
+   * Permite obtener todas las peliculas, ademas de filtrar por genero, pagina o limite
+   * @param {Object} obj
+   * @param {String} obj.genre
+   * @param {Number} obj.page
+   * @param {Number} obj.limit
+   * @returns {Promise<Array<Object>>} Array de peliculas
+   */
   static getAll = async ({ genre, page, limit }) => {
+    const peliculasConGeneros = await this.#getMoviGen();
     if (genre)
       return this.#getGenre({ genre, peliculasGenero: peliculasConGeneros });
     if (limit)
@@ -28,13 +45,27 @@ class MovieModel {
         peliculasGenero: peliculasConGeneros,
       });
 
-    const todos = await this.#getMoviGen();
-    return todos;
+    return peliculasConGeneros;
   };
 
+  /**
+   * Permite devolver las peliculas filtradas por un genero - PRIVADO
+   * @param {Object} obj
+   * @param {String} obj.genre - Genero de la pelicula
+   * @param {Array<Object>} obj.peliculasGenero - Array de peliculas con generos
+   * @returns {Array<Object>} Array de las peliculas filtradas por un genero
+   */
   static #getGenre = ({ genre, peliculasGenero }) =>
     peliculasGenero.filter((pelicula) => pelicula.genres.includes(genre));
 
+  /**
+   * Devuelve las peliculas segun la pagina y el limite - PRIVADO
+   * @param {Object} obj
+   * @param {Number} [obj.page]
+   * @param {Number} [obj.limit]
+   * @param {Array<Object>} obj.peliculasGenero
+   * @returns {Array<Object>}
+   */
   static #getLimit = ({ page = 1, limit = 10, peliculasGenero }) => {
     const limitT = page * limit;
     const result = peliculasGenero.slice(limitT - limit, limitT);
