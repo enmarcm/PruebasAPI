309b7708a712445b6d9adbba5da22c062e436747
enmarcm <theenmanuel123@gmail.com>
2023-09-17 13:31:08 -0400

    Router realizado

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -2,36 +2,36 @@
  * En esta clase se encontraran los metodos encargados de manejar las peticiones
  */
 class MovieController {
-  constructor({ movieMovel }) {
-    this.movieMovel = movieMovel;
+  constructor({ MovieModel }) {
+    this.movieModel = MovieModel;
   }
 
   getAll = async (req, res) => {
     const {limit, page, genre} = req.query
-    const resultado = await this.movieMovel.getAll({ limit, page, genre});
+    const resultado = await this.movieModel.getAll({ limit, page, genre});
     res.json(resultado);
   };
 
   //TODO: hacer resto de metodos
   getByID = async (req, res) => {
     const { id } = req.params;
-    const resultado = await this.movieMovel.getByID({ id });
+    const resultado = await this.movieModel.getByID({ id });
     res.json(resultado);
   };
   delete = async (req, res) => {
     const {id} = req.params
-    const resultado = await this.movieMovel.delete({ id })
+    const resultado = await this.movieModel.delete({ id })
     res.send(resultado)
   };
   update = async (req, res) => {
     const { id } = req.params
     const { movie } = req.body
-    const resultado = await this.movieMovel.update({ id, movie })
+    const resultado = await this.movieModel.update({ id, movie })
     res.send(resultado)
   };
   create = async (req, res) => {
     const { movie } = req.body
-    const resultado = await this.movieMovel.create({ movie })
+    const resultado = await this.movieModel.create({ movie })
     return res.json(resultado)
   };
   options = async (req, res, next) => {
--- routers/moviesRouter.js
+++ routers/moviesRouter.js
@@ -1,13 +1,18 @@
 import { Router } from "express";
-
+import MovieModel from "../models/local/movieModel.js";
+import MovieController from "../controllers/movieController.js";
 /**
  * Router que manejara las rutas de /movies
  * @type {Router} Router
  */
 const movieRouter = Router();
+const controller = new MovieController({ MovieModel });
 
-movieRouter.get("/", (req, res) => {
-  res.send("PAGINA DE PELICULAS");
-});
+movieRouter.get("/", controller.getAll);
+movieRouter.get("/:id", controller.getByID);
+movieRouter.delete("/:id", controller.delete);
+movieRouter.patch("/:id", controller.update);
+movieRouter.post("/", controller.create);
+movieRouter.options("/", controller.options);
 
 export default movieRouter;
